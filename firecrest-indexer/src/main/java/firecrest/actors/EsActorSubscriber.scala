cfcb8acec8c66edc5f1d025fded54fbe4e9cb652
Vasily Sulatskov <vasily@sulatskov.net>
2016-03-28 21:23:10 +0200

    Started working on an actor subscriber to insert messages into ES

---
+++ firecrest-indexer/src/main/java/firecrest/actors/EsActorSubscriber.scala
@@ -0,0 +1,96 @@
+package firecrest.actors
+
+import akka.actor.{Actor, ActorLogging, ActorRef}
+import akka.stream.actor.{ActorSubscriber, ActorSubscriberMessage, MaxInFlightRequestStrategy}
+import akka.util.ByteString
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.elasticsearch.client.support.AbstractClient
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
+
+object EsActorSubscriber {
+  case class Batch(messages: Seq[ByteString])
+  case class Done()
+}
+
+class EsActorSubscriber(client: AbstractClient) extends ActorSubscriber with ActorLogging {
+
+  import ActorSubscriberMessage._
+  import EsActorSubscriber._
+
+  var activeRequests = 0
+  val maxConcurrentRequests = 8
+
+  override val requestStrategy =
+    new MaxInFlightRequestStrategy(max = maxConcurrentRequests) {
+      override def inFlightInternally: Int = activeRequests
+    }
+
+
+  override def receive = {
+    case msg @ OnNext(Batch(messages)) =>
+      activeRequests += 1
+
+    case "tick" =>
+      log.info("tick")
+
+  }
+}
+
+class EsIndexWorker(timestampField: String, client: AbstractClient, mapper: ObjectMapper)
+  extends Actor with ActorLogging {
+
+  import EsActorSubscriber._
+
+  val timestampPath = "/" + timestampField
+  val indexNameFormatter = DateTimeFormat.forPattern("log-yyyy-mm-dd")
+
+  @throws[Exception](classOf[Exception])
+  override def preRestart(reason: Throwable, message: Option[Any]): Unit = {
+    log.error(reason, "Restarting: {}", message)
+    super.preRestart(reason, message)
+  }
+
+  override def receive = {
+    case Batch(messages) =>
+
+      log.info("Processing a batch")
+
+      try {
+        val bulkRequest = client.prepareBulk()
+
+        for (msg <- messages) {
+          indexName(msg) match {
+            case Some(index) =>
+              bulkRequest.add(client
+                .prepareIndex(index, "message")
+                .setSource(msg.toByteBuffer))
+
+            case None =>
+              log.warning(s"Unable to compute index name for '${msg.utf8String}'")
+          }
+        }
+        val bulkResponse = bulkRequest.get()
+
+        if (bulkResponse.hasFailures) {
+          log.error(s"Elasticsearch bulk request failures" +
+            s": ${bulkResponse.buildFailureMessage()}")
+        }
+      } finally {
+        sender ! Done()
+      }
+  }
+
+  def indexName(message: ByteString): Option[String] = {
+    val root = mapper.readTree(message.toArray)
+    val dateTimeText = root.at(timestampPath).asText()
+    val timestamp = try {
+      Some(DateTime.parse(dateTimeText))
+    } catch {
+      case IllegalArgumentException =>
+        None
+    }
+    timestamp.map(indexNameFormatter.print(_))
+  }
+
+}
